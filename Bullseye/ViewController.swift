f7c201880581ec3e24079b2f8d659144bc705d07
deejkelms <dkelmanson@gmail.com>
2017-10-02 17:33:18 -0400

    setup basic storyboard and connected all points to viewcontroller

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue = 0
+    @IBOutlet weak var slider: UISlider!
+    var targetValue = 0
+    @IBOutlet weak var targetLabel: UILabel!
+    var score = 0
+    @IBOutlet weak var scoreLabel: UILabel!
+    var round = 0
+    @IBOutlet weak var roundLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
+        startNewRound()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        round += 1
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +44,59 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func showAlert() {
+        
+        // abs is short for absolute value, converts negative to a positive
+        let difference = abs(targetValue - currentValue) // let is a const
+        
+        //   vs My code...
+        //        var difference: Int
+        //        if currentValue > targetValue {
+        //            difference = currentValue - targetValue
+        //        } else if targetValue > currentValue {
+        //            difference = targetValue - currentValue
+        //        } else {
+        //            difference = 0
+        //        }
+        
+        var points = 100 - difference
+        
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pretty good..."
+        } else {
+            title = "...Not even close"
+        }
+        
+        score += points
+        
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: {
+            action in
+                self.startNewRound()
+        })
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+
+    }
 
 }
 
