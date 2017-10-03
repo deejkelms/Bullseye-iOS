01e9cedb83b8485f8ed9a50a07dc89f0fe82bbe9
deejkelms <dkelmanson@gmail.com>
2017-10-03 13:05:17 -0400

    added info and about me view controllers

---
+++ Bullseye/AboutViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AboutViewController.swift
+//  Bullseye
+//
+//  Created by Derek Kelmanson on 10/3/17.
+//  Copyright © 2017 Derek Kelmanson. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
---
+++ Bullseye/AuthorViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AuthorViewController.swift
+//  Bullseye
+//
+//  Created by Derek Kelmanson on 10/3/17.
+//  Copyright © 2017 Derek Kelmanson. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+    
+    @IBAction func closeTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
