f74fa111c6d68ec374ead32a5d86e7dad54691f7
deejkelms <dkelmanson@gmail.com>
2017-10-04 21:29:30 -0400

    added final auto layout touches and added app icons

--- Bullseye/AboutViewController.swift
+++ Bullseye/AboutViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
     
     @IBAction func close() {
         dismiss(animated: true, completion: nil)
@@ -16,6 +19,13 @@ class AboutViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+            if let htmlData = try? Data(contentsOf: url) {
+                let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
+                webView.load(htmlData, mimeType: "text/html", characterEncodingName: "UTF-8", baseURL: baseURL)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
